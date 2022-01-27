c0949fc982bac6a03b6674f97c86b64d9f301aa3
Kaligula <ilya.kalganov.97@mail.ru>
2022-01-27 16:55:59 +0300

    [MODIFIED] add stack challenges

--- DataStructures&Algorithms.playground/Pages/Stacks.xcplaygroundpage/Contents.swift
+++ DataStructures&Algorithms.playground/Pages/Stacks.xcplaygroundpage/Contents.swift
@@ -63,8 +63,6 @@ example(of: "initializing a stack from an array literal") {
     stack.pop()
 }
 
-
-
 // MARK: Make stack initializable from an array literal
 extension Stack: ExpressibleByArrayLiteral {
     public init(arrayLiteral elements: Element...) {
@@ -82,3 +80,83 @@ extension Stack: CustomDebugStringConvertible {
         """
       }
 }
+
+// MARK: Challenges
+
+// Challenge #1
+// Create a function that uses a stack to print the contents of an array in reversed order
+func reverse1<T>(_ array: [T]) {
+    guard !array.isEmpty else { return }
+    
+    var newArray = array
+    var stack = Stack([])
+    
+    for _ in newArray {
+        stack.push(newArray.popLast()!)
+    }
+    
+    print(stack)
+}
+
+func reverse2<T>(_ array: [T]) {
+    var stack = Stack<T>()
+    
+    for value in array {
+        stack.push(value)
+    }
+    
+    while let value = stack.pop() {
+        print(value)
+    }
+}
+
+reverse1([1, 2, 3, 4])
+reverse2([1, 2, 3, 4])
+
+// Challenge #2
+// Check for balanced parentheses. Given a string, check if there are ( and ) characters, and return true if the parentheses in the string are balanced
+func checkParentheses1(_ string: String) -> Bool {
+    guard string != "" else { return true }
+    
+    var stack = Stack<Character>()
+    var count = 0
+    
+    for character in string {
+        stack.push(character)
+    }
+    
+    while let character = stack.pop() {
+        if character == ")" {
+            count += 1
+        } else if character == "(" {
+            count -= 1
+        }
+    }
+    
+    if count != 0 {
+        return false
+    }
+    
+    return true
+}
+
+func checkParentheses2(_ string: String) -> Bool {
+    var stack = Stack<Character>()
+    
+    for character in string {
+        if character == "(" {
+            stack.push(character)
+        } else if character == ")" {
+            if stack.isEmpty {
+                return false
+            } else {
+                stack.pop()
+            }
+        }
+    }
+    
+    return stack.isEmpty
+}
+
+print(checkParentheses1("(some)"))
+print(checkParentheses2("(some)"))
